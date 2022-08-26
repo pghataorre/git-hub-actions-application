01cc50e42f13e2c7f2f1b86dd8461656fdbc7e8c
Permy Ghataorre <pghataorre@and.digital>
2022-08-26 09:46:41 +0100

    main project set-up

--- git-up-actions/src/App.test.jsx
+++ git-up-actions/src/App.test.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,5 +7,9 @@ describe('renders learn react link', () => {
     render(<App />);
     const linkElement = screen.getByText(/learn react/i);
     expect(linkElement).toBeInTheDocument();
+  });
+
+  it('should run a tester test', () => {
+    expect(false).toEqual(false);
   });
 });
