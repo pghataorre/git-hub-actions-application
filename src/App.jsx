d3e2e0c25ab5142375a178ea3ba13fc6c3d90463
Permy Ghataorre <pghataorre@and.digital>
2022-09-20 18:14:37 +0100

    changed files

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Images from './components/Images/Images';
+import permyImage from './images/permy.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>PERMY.CO.UK - THIS GUY</h1>
+    <div class="image-container">
+          <Images src={permyImage}/>
+    </div>
     </div>
   );
 }
---
+++ src/components/Images/Images.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Images = ({src}) => (<img src={src} alt="Welcome" />)
+
+export default Images;
