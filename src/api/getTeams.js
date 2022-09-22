17889fdff6f095a16a6dd7d8161bd0220dcd2c15
Permy Ghataorre <pghataorre@and.digital>
2022-09-22 12:57:10 +0100

    testing ifg api works

--- src/api/getTeams.js
+++ src/api/getTeams.js
@@ -1,15 +1,15 @@
 const getTeams = async () => {
 
   const res = await fetch('https://hv19rmo72i.execute-api.us-east-1.amazonaws.com/live/teams', { 
-    mode: "cors",
-    headers: {
+    "mode": "cors",
+    "headers": {
       "Access-Control-Allow-Origin" : "https://www.permy.co.uk",
       "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
       "Access-Control-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS"
     }
   });
 
-    return res;
+  return res;
 }
 
 export default getTeams;
