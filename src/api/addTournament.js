abfd6729ac5231793b3ab184f956bb27f410069d
Permy Ghataorre <pghataorre@and.digital>
2022-10-10 21:03:00 +0100

    removed debugger

--- src/api/addTournament.js
+++ src/api/addTournament.js
@@ -1,8 +1,6 @@
 import config from '../config/config';
 
 const addTournament = async (body) => {
-  debugger;
-
   return await fetch(`${config.apiUrl}/addTournament`, {
     method: "POST",
     headers: {
