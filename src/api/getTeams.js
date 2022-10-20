51b6f857c69ae48b9b8eaea80bc99a511173124c
Permy Ghataorre <pghataorre@and.digital>
2022-10-20 11:28:20 +0100

    altered getTeams API

--- src/api/getTeams.js
+++ src/api/getTeams.js
@@ -1,7 +1,7 @@
 import config from '../config/config';
 
 const getTeams = async () => {
-  return await fetch(`${config.apiUrl}/teams`);
-}
+	return await fetch(`${config.apiUrl}/teams/`);
+};
 
 export default getTeams;
