b5b377f331960a957344b97f78ab12301e2aceb7
Permy Ghataorre <pghataorre@and.digital>
2022-10-31 15:10:29 +0000

    changed the managers endpoint path

--- src/api/addManager.js
+++ src/api/addManager.js
@@ -1,13 +1,13 @@
 import config from '../config/config';
 
 const addManager = async (body) => {
-  return await fetch(`${config.apiUrl}/addManager`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    },
-    body: JSON.stringify(body)
-  });
-}
+	return await fetch(`${config.apiUrl}/manager/createManager`, {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		body: JSON.stringify(body),
+	});
+};
 
 export default addManager;
--- src/api/getManagers.js
+++ src/api/getManagers.js
@@ -1,7 +1,7 @@
 import config from '../config/config';
 
 const getManagers = async (teamId) => {
-  return await fetch(`${config.apiUrl}/getManager/${teamId}`);
-}
+	return await fetch(`${config.apiUrl}/manager/getManager/${teamId}`);
+};
 
 export default getManagers;
