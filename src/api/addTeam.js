e392f641eac8c33d73974c77fdac85b6110e2b5e
Permy Ghataorre <pghataorre@and.digital>
2022-10-31 15:30:07 +0000

    changed the teams endpoints

--- src/api/addTeam.js
+++ src/api/addTeam.js
@@ -1,13 +1,13 @@
 import config from '../config/config';
 
 const addTeam = async (body) => {
-  return await fetch(`${config.apiUrl}/addTeam`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    },
-    body: JSON.stringify(body)
-  });
-}
+	return await fetch(`${config.apiUrl}/teams/createTeam`, {
+		method: 'POST',
+		headers: {
+			'Content-type': 'application/json; charset=UTF-8',
+		},
+		body: JSON.stringify(body),
+	});
+};
 
 export default addTeam;
--- src/api/getTeams.js
+++ src/api/getTeams.js
@@ -2,7 +2,7 @@ import config from '../config/config';
 
 const getTeams = async (tournamnetId) => {
 	return await fetch(
-		`${config.apiUrl}/teams/${!tournamnetId ? '' : tournamnetId}`
+		`${config.apiUrl}/teams/getTeams/${!tournamnetId ? '' : tournamnetId}`
 	);
 };
 
