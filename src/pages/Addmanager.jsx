e50925853b7372a0fe066b4c0ffd85e25122e534
Permy Ghataorre <pghataorre@and.digital>
2022-11-02 13:43:56 +0000

    fix build attempt

--- src/pages/Addmanager.jsx
+++ src/pages/Addmanager.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import AddManagerForm from '../components/ManagerForm/ManagerForm';
+import ManagerForm from '../components/ManagerForm/ManagerForm';
 
 const AddManager = () => {
   return (
     <div className="admin-page">
       <h1>Admin:: Add a Football team manager</h1>
       <p>Please fill in the details below to add a manager</p>
-      <AddManagerForm />
+      <ManagerForm />
     </div>
   );
 }
